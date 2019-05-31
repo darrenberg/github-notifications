2c53c49c1309c40421699e266b186ae90f9b34f7
Darren Berg <darren.berg@doctolib.com>
2019-05-31 15:17:29 +0200

    initial commit

---
+++ notify.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+USER=$(git config user.email)
+TOKEN=$(security find-generic-password -s github_token -w)
+
+curl -s -u $USER:$TOKEN  https://api.github.com/notifications \
+  | jq -r '.[] | select(.unread) | [.subject.type, .reason, .subject.title] | @sh' \
+  | xargs -n 3 sh -c \
+  'terminal-notifier -title "GitHub Notification" -subtitle "$0 $1" -message "$2" -open "https://github.com/notifications"'
