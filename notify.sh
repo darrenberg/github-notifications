99724c4210b03470e3af09d0ee6365640ff56248
David Slabaugh <dslabaugh@covermymeds.com>
2020-01-16 15:43:14 -0500

    switched to github enterprise.

--- notify.sh
+++ notify.sh
@@ -5,9 +5,9 @@ TOKEN=$(security find-generic-password -a github_token -w)
 
 while true
 do 
-  curl -s -u $USER:$TOKEN  https://api.github.com/notifications \
+  curl -s -u $USER:$TOKEN  https://git.innova-partners.com/api/v3/notifications \
     | jq -r '.[] | select(.unread) | [.subject.type, .reason, .subject.title] | @sh' \
     | xargs -n 3 sh -c \
-    'terminal-notifier -title "GitHub Notification" -subtitle "$0 $1" -message "$2" -open "https://github.com/notifications"'
+    'terminal-notifier -title "GitHub Notification" -subtitle "$0 $1" -message "$2" -open "https://git.innova-partners.com/notifications"'
   sleep $(($1 * 60))
 done
